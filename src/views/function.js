92d2ef69a81e283f902b509f2c1e95dea11b0019
Edyta Bartnik <56174484+3Bartnik@users.noreply.github.com>
2020-04-19 14:34:39 +0200

    Create function.js

---
+++ src/views/function.js
@@ -0,0 +1,24 @@
+
+/*
+function toggleClass(){
+    var element = document.getElementsByClassName("room1");
+    if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+};
+export default toggleClass;
+
+*/
+
+window.addEventListener("click", function toggleClass(){
+    var element = document.getElementsByClassName("room1");
+        if (element.style.display === "none") {
+        element.style.display = "block";
+      } else {
+        element.style.display = "none";
+      }
+    })
+
+    export default toggleClass;
